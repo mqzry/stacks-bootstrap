e992c0fcf809c202d16e232967437f4565b95bb7
Mohamed Hashi <mhas@spreadshirt.net>
2017-05-18 10:30:07 +0200

    Fixed some bugs in bootstrap script

--- bootstrap.py
+++ bootstrap.py
@@ -1,34 +1,51 @@
-from os import path
+import sys
+from os.path import join, dirname, realpath
+import re
 
 def replaceProjectPath(path):
-	f = open("config.ini", "w")
-	lines = f.readlines()
-	for line in lines:
-		if "project" in line:
-			line = "project = \"{}\"".format(path)
-		f.write(line + "\n")
-
-	f.close()
+	replaceFileContent('config.ini', r'project = "[^\"]*"', 'project = "{}"'.format(path))
 
 def replaceHost(host):
 	replaceFileContent("tex/scripts/tag_up.py", "http://stacks.math.columbia.edu/tag/", host)
 
-def bootstrapXyJax:
-	searchString = 'MathJax.Ajax.loadComplete("[MathJax]/extensions/TeX/xypic.js");'
-	replaceString = 'MathJax.Ajax.loadComplete("/js/XyJax/extensions/TeX/xypic.js");'
+def bootstrapXyJax():
+	searchString = "MathJax.Ajax.loadComplete(\"[MathJax]/extensions/TeX/xypic.js\");"
+	replaceString = "MathJax.Ajax.loadComplete(\"/js/XyJax/extensions/TeX/xypic.js\");"
 	replaceFileContent("js/XyJax/extensions/TeX/xypic.js", searchString, replaceString)
 
-def replaceFileContent(path, searchString, replaceString):
-	f = open(path, "w")
+def replacePDFLATEXCommand():
+	replaceFileContentRegex('tex/tags/Makefile', r"PDFLATEX := .*", "PDFLATEX := pdflatex")
+
+def replaceFileContent(relpath, searchString, replaceString):
+	path = join(bootstrapRoot, relpath)
+	print(path)
+	f = open(path, "r")
 	content = f.read()
-	content.replace(searchString, replaceString)
+	f.close()
+	content = content.replace(searchString, replaceString)
+	f = open(path, "w")	
+	f.write(content)
+	f.close()
+
+def replaceFileContentRegex(relpath, regex, replaceString):
+	path = join(bootstrapRoot, relpath)
+	f = open(path, "r")
+	content = f.read()
+	# print content
+	f.close()
+	content = re.sub(regex, replaceString, content)
+	# print content
+	f = open(path, "w")	
 	f.write(content)
 	f.close()
 
 
-host = argv[1]
-currentpath = os.path.dirname(sys.argv[0])
 
-replaceProjectPath(join(currentpath, 'stacks-website'))
-replaceHost(host)
-bootstrapXyJax()
+host = sys.argv[1]
+currentpath = dirname(sys.argv[0])
+bootstrapRoot = dirname(realpath('__file__'))
+
+# replaceProjectPath(join(currentpath, 'stacks-website'))
+# replaceHost(host)
+# bootstrapXyJax()
+replacePDFLATEXCommand()
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,31 @@
-host = $1
+#!/usr/bin/env bash
+path_to_caddy=$(which caddy)
+if [ -x "$path_to_caddy" ] ; then
+	echo "Caddy bin already here: $path_to_caddy"
+else
+	curl https://getcaddy.com | bash
+fi
 
 git submodule init
 git submodule update
 
-python bootstrap.py $host
+
+
+cd stacks-website
+git submodule init
+git submodule update
+if [ ! -d "tex" ] ; then
+	git clone git://github.com/stacks/stacks-project tex
+fi
+
+
+python ../bootstrap.py $1
+
+cd tex
+rm -rf tags/tmp/*
 make tags
 
-cd ..
-
-git clone https://github.com/stacks/stacks-tools
-cd stacks-tools
+cd ../../stacks-tools
 
 python create.py
 cd ..
