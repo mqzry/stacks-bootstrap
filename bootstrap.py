a6e6a593d724ec3abbaef90d9a346be0e234b3fd
Mohamed Hashi <mhas@spreadshirt.net>
2017-06-08 20:11:18 +0200

    extracted webserver docker image and removed ssh git links

--- bootstrap.py
+++ bootstrap.py
@@ -7,12 +7,8 @@ def bootstrapXyJax():
 	replaceString = "MathJax.Ajax.loadComplete(\"/js/XyJax/extensions/TeX/xypic.js\");"
 	replaceFileContent("js/XyJax/extensions/TeX/xypic.js", searchString, replaceString)
 
-def replacePDFLATEXCommand():
-	replaceFileContentRegex('tex/tags/Makefile', r"PDFLATEX := .*", "PDFLATEX := pdflatex")
-
 def replaceFileContent(relpath, searchString, replaceString):
 	path = join(bootstrapRoot, relpath)
-	print(path)
 	f = open(path, "r")
 	content = f.read()
 	f.close()
@@ -35,7 +31,7 @@ def replaceFileContentRegex(relpath, regex, replaceString):
 if len(sys.argv) == 2:
 	host = sys.argv[1]
 else:
-	host = "localhost/tag"
+	host = "localhost/tag/"
 
 currentpath = dirname(sys.argv[0])
 bootstrapRoot = dirname(realpath('__file__'))
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,26 @@ if [ ! -d "tex" ] ; then
 	git clone https://github.com/stacks/stacks-project tex
 fi
 
-ln -s css/stacks-editor.css js/EpicEditor/epiceditor/themes/editor/stacks-editor.css
-ln -s css/stacks-preview.css js/EpicEditor/epiceditor/themes/editor/stacks-preview.css
+ln -s css/stacks-editor.css js/EpicEditor/epiceditor/themes/editor/stacks-editor.css > /dev/null 2>&1
+ln -s css/stacks-preview.css js/EpicEditor/epiceditor/themes/editor/stacks-preview.css > /dev/null 2>&1
 
-python ../bootstrap.py $1
 
-cd tex
-rm -rf tags/tmp/*
-make tags
-cd ../..
+read -p "What hostname should be used?
+" host
 
+docker run -d -p 8080:80 -v $PWD:/var/www/html -t sadar/stacks-php > /dev/null 2>&1
+
+python ../bootstrap.py ${host}
+
+echo "Compiling tex"
+rm -rf tex/tags/tmp/* > /dev/null 2>&1
+docker run -v $PWD/tex:/tex -it sadar/stacks-texlive /bin/bash -c "cd tex && make tags"
+
+cd ..
 ./initDB.sh
 
+echo "Generating graphs"
 cd stacks-tools
-mkdir ../stacks-website/data
+mkdir ../stacks-website/data > /dev/null 2>&1
 python graphs.py
+cd ..
